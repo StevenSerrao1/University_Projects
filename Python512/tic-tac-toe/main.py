4d7159b083ac0bdbfb04111fd6702dc7618f6357
Steven Serrao <stevenserraowork@gmail.com>
2025-10-04 07:35:49 +0200

    first real commit

---
+++ Python512/tic-tac-toe/main.py
@@ -0,0 +1,3 @@
+﻿
+print('========== Welcome to TIC-TAC-TOE ==========\n---------------------------------------------\n\n')
+vs_choice = input('Choose your playing mode:\n1.) Player vs. Player\n2.) ')
